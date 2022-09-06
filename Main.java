e5eadf285ea98532f85d2b58501f5019fd3e9783
Blvck-Devil <112816848+Blvck-Devil@users.noreply.github.com>
2022-09-06 14:48:32 +0000

    2

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int i,j;
+    int rows=5;
+    for(i=0;i<=5;i++){
+      for(j=0; j<5; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+
+    for ( i = 1; i <= rows; ++i) {
+      for ( j = 1; j <= i; ++j) {
+        System.out.print(" * ");
+      }
+      System.out.println();
+    }
+    
   }
 }
